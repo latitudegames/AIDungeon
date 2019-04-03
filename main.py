51aecec19c3834adf4f72fd13407a50fe4f749af
Nick Walton <nickwalton00@gmail.com>
2019-04-02 20:50:39 -0600

    terminal look

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ def root():
                    datetime.datetime(2018, 1, 3, 11, 0, 0),
                    ]
 
-    return render_template('index.html', times=dummy_times)
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
