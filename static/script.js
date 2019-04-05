ecc8d12deea0d9f7b2d32b41418f3b26dd7f147c
Nick Walton <nickwalton00@gmail.com>
2019-04-04 23:59:27 -0600

    made code simpler and separated

--- static/script.js
+++ static/script.js
@@ -1,24 +1,112 @@
-/**
- * Copyright 2018, Google LLC
- * Licensed under the Apache License, Version 2.0 (the `License`);
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an `AS IS` BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+var Typer={
+	text: null,
+	accessCountimer:null,
+	index:0, 
+	speed:2,
+	file:"", 
+	accessCount:0,
+	deniedCount:0, 
+	init: function(){
+		accessCountimer=setInterval(function(){Typer.updLstChr();},500); 
+		$.get(Typer.file,function(data){
+			Typer.text=data;
+			Typer.text = Typer.text.slice(0, Typer.text.length-1);
+		});
+	},
  
-// [START gae_python37_log]
-'use strict';
+	content:function(){
+		return $("#console").html();
+	},
+ 
+	write:function(str){
+		$("#console").append(str);
+		return false;
+	},
+ 
+	addText:function(key){
+		
+		if(key.keyCode==18){
+			Typer.accessCount++; 
+			
+			if(Typer.accessCount>=3){
+				Typer.makeAccess(); 
+			}
+		}
+		
+    		else if(key.keyCode==20){
+			Typer.deniedCount++; 
+			
+			if(Typer.deniedCount>=3){
+				Typer.makeDenied(); 
+			}
+		}
+		
+    		else if(key.keyCode==27){ 
+			Typer.hidepop(); 
+		}
+		
+    		else if(Typer.text){ 
+			var cont=Typer.content(); 
+			if(cont.substring(cont.length-1,cont.length)=="|") 
+				$("#console").html($("#console").html().substring(0,cont.length-1)); 
+			if(key.keyCode!=8){ 
+				Typer.index+=Typer.speed;	
+			}
+      		else {
+			if(Typer.index>0) 
+				Typer.index-=Typer.speed;
+			}
+			var text=Typer.text.substring(0,Typer.index)
+			var rtn= new RegExp("\n", "g"); 
+	
+			$("#console").html(text.replace(rtn,"<br/>"));
+			window.scrollBy(0,50); 
+		}
+		
+		if (key.preventDefault && key.keyCode != 122) { 
+			key.preventDefault()
+		};  
+		
+		if(key.keyCode != 122){ // otherway prevent keys default behavior
+			key.returnValue = false;
+		}
+	},
+ 
+	updLstChr:function(){ 
+		var cont=this.content(); 
+		
+		if(cont.substring(cont.length-1,cont.length)=="|") 
+			$("#console").html($("#console").html().substring(0,cont.length-1)); 
+		
+		else
+			this.write("|"); // else write it
+	}
+}
+ 
+function replaceUrls(text) {
+	var http = text.indexOf("http://");
+	var space = text.indexOf(".me ", http);
+	
+	if (space != -1) { 
+		var url = text.slice(http, space-1);
+		return text.replace(url, "<a href=\""  + url + "\">" + url + "</a>");
+	} 
+	
+	else {
+		return text
+	}
+}
 
-window.addEventListener('load', function () {
-
-  console.log("Hello World!");
-
-});
-// [END gae_python37_log]
+Typer.speed=1;
+Typer.file="static/dungeondream.txt";
+Typer.init();
+ 
+var timer = setInterval("t();", 30);
+function t() {
+	Typer.addText({"keyCode": 123748});
+	
+	if (Typer.index > Typer.text.length) {
+		clearInterval(timer);
+	}
+}
+ 
