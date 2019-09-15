a0c6934aaecf551de77f21a697e2ce7c5aeb99fe
Nick <nickwalton00@gmail.com>
2019-09-14 19:53:11 -0600

    made significant rework. Console version works unconstrained

--- console_play.py
+++ console_play.py
@@ -3,22 +3,28 @@ from story.utils import *
 from google.cloud import storage
 import json
 from story.story_manager import *
-from generator.tf_local.generator_local import *
+from generator.web.web_generator import *
 import tensorflow as tf
+import textwrap
+
+
+# Set the key
+def console_print(str):
+    LINE_WIDTH=80
+    print((textwrap.fill(str, 80)))
+
 
 if __name__ == '__main__':
-
-    sess = tf.Session()
-    generator = LocalGenerator(sess)
+    generator = WebGenerator("./AI-Adventure-2bb65e3a4e2f.json")
     prompt = "You enter a dungeon with your trusty sword and shield. You are searching for the evil necromancer who killed your family. You've heard that he resides at the bottom of the dungeon, guarded by legions of the undead. You enter the first door and see"
     story_manager = UnconstrainedStoryManager(generator, prompt)
-    print(story_manager.story)
+
+    console_print(str(story_manager.story))
     while(True):
         action = input("> ")
         action = "You " + action
-        print(action)
         result = story_manager.act(action)
-        print(result)
+        console_print(action + result)
 
 
 
