9d763e2afb5efd114027d7494831a624801b3823
Nick <nickwalton00@gmail.com>
2019-09-20 17:06:01 -0600

    Update

--- generator/ctrl/ctrl_generator.py
+++ generator/ctrl/ctrl_generator.py
@@ -265,7 +265,7 @@ class CTRLGenerator():
             # like which one was chosen,
             # what the top25 were,
             # here is your opportunity.
-            print('chosen:', repr(self.idx2word[idx]))
+            #print('chosen:', repr(self.idx2word[idx]))
             # print('top25 alternatives:', pruned_list[:25])
 
             if self.idx2word[idx] == "\n":
@@ -280,6 +280,7 @@ class CTRLGenerator():
             tokens_generated_so_far = ' '.join([self.idx2word[c] for c in tokens_generated[0].squeeze()[:token + 2]])
             tokens_generated_so_far = re.sub('(@@ )', '', string=tokens_generated_so_far)
             tokens_generated_so_far = re.sub('(@@ ?$)', '', string=tokens_generated_so_far)
+            print(tokens_generated_so_far)
 
             result = tokens_generated_so_far[prompt_length:]
             first_token = False
