d78600182e1f91625535718efb2adcdf427c2c7c
Nick <nickwalton00@gmail.com>
2019-09-24 12:27:25 -0600

    small update

--- console_play.py
+++ console_play.py
@@ -4,7 +4,7 @@ from google.cloud import storage
 import json
 from story.story_manager import *
 from generator.web.web_generator import *
-#from generator.ctrl.ctrl_generator import *
+from generator.ctrl.ctrl_generator import *
 import tensorflow as tf
 import textwrap
 
@@ -90,7 +90,7 @@ def play_cached():
 
 
 if __name__ == '__main__':
-    play_constrained()
+    play_unconstrained()
 
 
 
