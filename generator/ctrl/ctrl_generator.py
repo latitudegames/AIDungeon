7f9ef794dcb98fe014595714433c34ff7ea37f80
Nick Walton <nickwalton00@gmail.com>
2019-09-27 12:21:53 -0600

    update

--- generator/ctrl/ctrl_generator.py
+++ generator/ctrl/ctrl_generator.py
@@ -20,7 +20,7 @@ def loss(labels, logits):
 
 class CTRLGenerator():
 
-    def __init__(self, control_code="Horror Text: ", generate_num=80, temperature=0.4):
+    def __init__(self, control_code="Books: ", generate_num=80, temperature=0.4):
 
         self.generate_num=generate_num
         model_dir = "generator/ctrl/model/seqlen256_v1.ckpt/"
@@ -158,7 +158,7 @@ class CTRLGenerator():
         if prompt[-1] != " ":
             prompt = prompt + " "
 
-        #prompt = second_to_first_person(prompt)
+        prompt = second_to_first_person(prompt)
 
         prompt = self.control_code + prompt
         # print("\n\nAFTER PROMPT_REPLACE")
@@ -218,7 +218,7 @@ class CTRLGenerator():
         # disallow some tokens
         forbidden_tokens = ['<unk>', 'Sco@@', "&amp@@", "1]@@", "2]@@", "3]@@", "4]@@", "https://www.@@", "[@@", ":@@",
                             "Edit", "&@@", "2:","1:", ":", "Edit@@", "EDI@@", "EDIT@@", "edit", "TL@@", "tl@@", ";@@",
-                            '**', "http://@@", "Redd@@", "UP@@"]
+                            '**', "http://@@", "Redd@@", "UP@@", "mom"]
 
         if num_new_lines > self.max_new_lines:
             forbidden_tokens.append("\n")
