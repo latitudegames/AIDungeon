f1e85d534db239c3ae67a18ce1e445120f4e8693
Nick Walton <nickwalton00@gmail.com>
2019-09-27 12:59:24 -0600

    update

--- console_play.py
+++ console_play.py
@@ -23,7 +23,7 @@ def console_print(str, pycharm=False):
 def play_unconstrained():
     generator = CTRLGenerator()
     #generator = WebGenerator(CRED_FILE)
-    prompt = get_story_start("vague_police")
+    prompt = get_story_start("hospital")
     story_manager = UnconstrainedStoryManager(generator)
     story_manager.start_new_story(prompt)
 
