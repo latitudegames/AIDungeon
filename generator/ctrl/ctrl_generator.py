b8cf5fccc897531245ff5af93202bb0c088c852c
Nick <nickwalton00@gmail.com>
2019-09-26 19:59:57 -0600

    update

--- generator/ctrl/ctrl_generator.py
+++ generator/ctrl/ctrl_generator.py
@@ -217,7 +217,7 @@ class CTRLGenerator():
 
         # disallow some tokens
         forbidden_tokens = ['<unk>', 'Sco@@', "&amp@@", "1]@@", "2]@@", "3]@@", "4]@@", "https://www.@@", "[@@", ":@@",
-                            "Edit", "&@@", "2:","1:", ":", "Edit@@", "EDI@@", "EDIT@@"]
+                            "Edit", "&@@", "2:","1:", ":", "Edit@@", "EDI@@", "EDIT@@", "edit", "TL@@"]
 
         if num_new_lines > self.max_new_lines:
             forbidden_tokens.append("\n")
