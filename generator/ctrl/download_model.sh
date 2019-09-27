af2a10f826235a38a3e5d20ee5528801ddb7227f
Nick Walton <nickwalton00@gmail.com>
2019-09-27 14:23:18 -0600

    update

--- generator/ctrl/download_model.sh
+++ generator/ctrl/download_model.sh
@@ -2,12 +2,7 @@ curl https://sdk.cloud.google.com | bash
 exec -l $SHELL
 gcloud init
 
-# Download the 512-length model if specified, 256-length otherwise
-if [ "$1" = "512" ]
-then
-    URL="gs://sf-ctrl/seqlen512_v1.ckpt/"
-else
-    URL="gs://sf-ctrl/seqlen256_v1.ckpt/"
-fi
+
+URL="gs://sf-ctrl/seqlen512_v1.ckpt/"
 
 gsutil -m cp -r "$URL" model
