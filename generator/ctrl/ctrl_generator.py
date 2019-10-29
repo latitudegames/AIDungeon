105bbfd856be3afa93799d2b26ba2544fa485c9e
Nick <nickwalton00@gmail.com>
2019-10-29 15:00:44 -0600

    update

--- generator/ctrl/ctrl_generator.py
+++ generator/ctrl/ctrl_generator.py
@@ -20,7 +20,7 @@ def loss(labels, logits):
 
 class CTRLGenerator():
 
-    def __init__(self, control_code="Writing Text: ", generate_num=45, temperature=0.4, topk=40, nucleus_prob=0):
+    def __init__(self, control_code="Writing Text: ", generate_num=40, temperature=0.3, topk=20, nucleus_prob=0):
 
         self.generate_num=generate_num
         model_dir = "generator/ctrl/training_utils/seqlen256_v1.ckpt/"
