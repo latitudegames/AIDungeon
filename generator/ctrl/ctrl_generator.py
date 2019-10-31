b9937218d45b66097ecc90c6c8ecd5da5918f881
Nick <nickwalton00@gmail.com>
2019-10-31 09:26:03 -0600

    update

--- generator/ctrl/ctrl_generator.py
+++ generator/ctrl/ctrl_generator.py
@@ -311,8 +311,6 @@ class CTRLGenerator():
             if debug_print:
                 print(repr(self.idx2word[idx]), end="_")
 
-            import pdb
-            pdb.set_trace()
             tokens_generated_so_far = ' '.join([self.idx2word[c] for c in tokens_generated[0][len(text):token+2]])
             tokens_generated_so_far = re.sub('(@@ )', '', string=tokens_generated_so_far)
             tokens_generated_so_far = re.sub('(@@ ?$)', '', string=tokens_generated_so_far)
