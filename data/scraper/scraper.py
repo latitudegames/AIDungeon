c1209949354e14b26eaf7ca0482029166013d4c7
Nick <nickwalton00@gmail.com>
2019-11-15 15:51:47 -0700

    updated version of scraper

--- data/scraper/scraper.py
+++ data/scraper/scraper.py
@@ -1,7 +1,23 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import time
+import json
+"""
+format of tree is
+dict {
+    tree_id: tree_id_text
+    context: context text?
+    first_story_block
+    action_results: [act_res1, act_res2, act_res3...]
+}
 
+where each action_result's format is:
+dict{
+    action: action_text
+    result: result_text
+    action_results: [act_res1, act_res2, act_res3...]
+}
+"""
 
 class Scraper:
 
@@ -12,13 +28,15 @@ class Scraper:
         chrome_options.add_argument("--window-size=1920x1080")
         exec_path = "/usr/bin/chromedriver"
         self.driver = webdriver.Chrome(chrome_options=chrome_options, executable_path=exec_path)
-        self.max_depth = 20
+        self.max_depth = 10
+        self.end_action = "End Game and Leave Comments"
+        self.texts = set()
 
     def GoToURL(self, url):
         self.driver.get(url)
         time.sleep(0.5)
 
-    def GatherText(self):
+    def GetText(self):
         div_elements = self.driver.find_elements_by_css_selector("div")
         text = div_elements[3].text
         return text
@@ -27,23 +45,82 @@ class Scraper:
         return self.driver.find_elements_by_css_selector("a")
 
     def GoBack(self):
-        self.GetLinks().click()
+        self.GetLinks()[0].click()
+        time.sleep(0.2)
 
-    def ClickAction(self, action_num):
-        link = self.GetLinks()[action_num+4]
-        if link.text != "self.GetLinks()[action_num+4]":
-            self.GetLinks()[action_num+4].click()
-        else:
-            print("at the end so can no longer go forward")
+    def ClickAction(self, links, action_num):
+        links[action_num+4].click()
+        time.sleep(0.2)
+
+    def GetActions(self):
+        return [link.text for link in self.GetLinks()[4:]]
 
     def NumActions(self):
         return len(self.GetLinks()) - 4
 
+    def BuildTreeHelper(self, parent_story, action_num, depth, old_actions):
+        depth += 1
+        action_result = {}
+
+        action = old_actions[action_num]
+        print("Action is ", action)
+        action_result["action"] = action
+
+        links = self.GetLinks()
+        if action_num+4 >= len(links):
+            return None
+
+        self.ClickAction(links, action_num)
+        result = self.GetText()
+        if result == parent_story or result in self.texts:
+            self.GoBack()
+            return None
+
+        self.texts.add(result)
+        print(len(self.texts))
+
+        action_result["result"] = result
+
+        actions = self.GetActions()
+        action_result["action_results"] = []
+
+        for i, action in enumerate(actions):
+            if actions[i] != self.end_action and actions[i] != "Epilogue":
+                sub_action_result = self.BuildTreeHelper(result, i, depth, actions)
+                if action_result is not None:
+                    action_result["action_results"].append(sub_action_result)
+
+        self.GoBack()
+        return action_result
+
+
+    def BuildStoryTree(self, url):
+        scraper.GoToURL(url)
+        text = scraper.GetText()
+        actions = self.GetActions()
+        story_dict = {}
+        story_dict["tree_id"] = url
+        story_dict["context"] = ""
+        story_dict["first_story_block"] = text
+        story_dict["action_results"] = []
+
+        for i, action in enumerate(actions):
+            if action != self.end_action:
+                action_result = self.BuildTreeHelper(text, i, 0, actions)
+                if action_result is not None:
+                    story_dict["action_results"].append(action_result)
+
+        return story_dict
+
+def save_tree(tree, filename):
+    with open(filename, 'w') as fp:
+        json.dump(tree, fp)
+
 scraper = Scraper()
+
 urls = ["http://chooseyourstory.com/story/viewer/default.aspx?StoryId=10638",
         "http://chooseyourstory.com/story/viewer/default.aspx?StoryId=11246"]
-scraper.GoToURL(urls[1])
-scraper.ClickAction(0)
-text = scraper.GatherText()
-links = scraper.GetLinks()
-print("Done")
+
+tree = scraper.BuildStoryTree(urls[0])
+save_tree(tree, "scraped_story.json")
+print("done")
