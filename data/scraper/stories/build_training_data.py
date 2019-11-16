dac9605ada9eecefc1e956e2775a7cf2943c421b
Nick <nickwalton00@gmail.com>
2019-11-15 18:28:51 -0700

    made training data

---
+++ data/scraper/stories/build_training_data.py
@@ -0,0 +1,44 @@
+import csv
+import json
+
+
+def load_tree(filename):
+    with open(filename, 'r') as fp:
+        tree = json.load(fp)
+    return tree
+
+def make_stories(current_story, tree):
+    stories = []
+    current_story += ("\n> " + tree["action"] + "\n" + tree["result"])
+
+    action_results = tree["action_results"]
+    if len(action_results) == 0 or action_results[0] is None:
+        return [current_story]
+    else:
+        stories += make_stories(current_story, action_results[0])
+
+        for i in range(1, len(action_results)):
+            if action_results[i] is not None:
+                stories += make_stories(tree["result"], action_results[i])
+
+    return stories
+
+def get_stories(filename):
+    tree = load_tree(filename)
+    stories = []
+    for action_result in tree["action_results"]:
+        stories += make_stories(tree["first_story_block"], action_result)
+    return stories
+
+
+output_file_path = "text_adventures.csv"
+with open(output_file_path, 'w') as output_file:
+    writer = csv.writer(output_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+    filenames = ["story0.json"]
+    stories = []
+    for filename in filenames:
+        stories += get_stories(filename)
+
+    for story in stories:
+        writer.writerow([story])
+
