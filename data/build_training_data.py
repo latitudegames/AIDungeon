6b68127f7590e49c0d2caf7fbcfa8ea3e376f1a3
Nick Walton <nickwalton00@gmail.com>
2019-11-18 12:26:54 -0700

    removed old web stuff

--- data/build_training_data.py
+++ data/build_training_data.py
@@ -33,8 +33,8 @@ def get_stories(filename):
 
 output_file_path = "text_adventures.txt"
 with open(output_file_path, 'w') as output_file:
-    filenames = ["story" + str(i) + ".json" for i in range(0,69)]
-    filenames += ["apoc_seed" + str(i) + ".json" for i in range(1,5)]
+    filenames = ["stories/story" + str(i) + ".json" for i in range(0,69)]
+    filenames += ["stories/apoc_seed" + str(i) + ".json" for i in range(1,5)]
     stories = []
     for filename in filenames:
         stories += get_stories(filename)
