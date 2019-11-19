2edb9d46c2f5c80f8349c364424df4be5c978a25
Nick Walton <nickwalton00@gmail.com>
2019-11-18 17:02:36 -0700

    added install script

--- download_model.sh
+++
@@ -1,1 +0,0 @@
-gsutil -m cp -r gs://aidungeon2model/model_v1 ./aidungeon/generator/gpt2/models
---
+++ install.sh
@@ -0,0 +1,10 @@
+MODEL_DIRECTORY=aidungeon/generator/gpt2/models/model_v1
+
+if [ -d "$MODEL_DIRECTORY" ]; then
+    echo "AIDungeon is already installed"
+
+else
+    echo "Installing AIDungeon"
+    gsutil -m cp -r gs://aidungeon2model/model_v1 ./aidungeon/generator/gpt2/models
+    pip install -r requirements.txt
+fi
