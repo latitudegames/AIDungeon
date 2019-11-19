6169722df66cfaca07ef75f96923763bd0d448b5
Nick Walton <nickwalton00@gmail.com>
2019-11-19 13:01:43 -0700

    update

--- play.py
+++ play.py
@@ -1,54 +1,34 @@
 from story.story_manager import *
 from generator.gpt2.gpt2_generator import *
-
-def console_print(text, width=75):
-    last_newline = 0
-    i = 0
-    while i < len(text):
-        if text[i] == "\n":
-            last_newline = 0
-        elif last_newline > width and text[i] == " ":
-            text = text[:i] + "\n" + text[i:]
-            last_newline = 0
-        else:
-            last_newline += 1
-        i += 1
-    print(text)
-
-
-def get_num_options(num):
-
-    while True:
-        choice = input("Which do you choose? ")
-        try:
-            result = int(choice)
-            if result >= 0 and result < num:
-                return result
-            else:
-                print("Error invalid choice. ")
-        except ValueError:
-            print("Error invalid choice. ")
-
+from story.utils import *
+from story.custom_story import *
 
 def select_game():
     print("Which game would you like to play?")
-    options = ["zombies", "hospital", "peasant", "apocalypse", "classic", "knight", "necromancer", "vague"]
+    options = ["zombies", "hospital", "apocalypse", "classic", "knight", "necromancer", "custom"]
     for i, option in enumerate(options):
         console_print(str(i) + ") " + option + ": " + get_context(option) + "\n")
 
     choice = get_num_options(len(options))
-    return options[choice]
+    if options[choice] == "custom":
+        context, prompt = make_custom_story()
+
+    else:
+        game = options["choice"]
+        prompt = get_story_start(game)
+        context = get_context(game)
+
+    return context, prompt
 
 def instructions():
     text = "\nAI Dungeon 2 Instructions:"
     text += '\n* Enter actions starting with a verb ex. "go to the tavern" or "attack the orc."'
-    text += '\n* If you want to say something then enter \'say "(thing you want to say)"\'.'
-    text += '\n* You can also just press enter with no action if you want to let the story continue.'
+    text += '\n* If you want to say something then enter \'say "(thing you want to say)"\''
     text += '\n* Finally if you want to end your game and start a new one just enter "restart" for any action. '
     return text
 
-def play_aidungeon_2():
 
+def play_aidungeon_2():
 
     print("Initializing AI Dungeon! (This might take a few minutes)")
     generator = GPT2Generator()
@@ -62,12 +42,10 @@ def play_aidungeon_2():
     while True:
 
         print("\n\n")
-        game = select_game()
+        context, prompt = select_game()
         console_print(instructions())
-        prompt = get_story_start(game)
-        context = get_context(game)
-        story_manager.start_new_story(prompt, context=context)
 
+        story_manager.start_new_story(prompt, context=context)
 
         print("\n")
         console_print(context + str(story_manager.story))
