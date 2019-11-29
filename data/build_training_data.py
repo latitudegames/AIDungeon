fe66a88a99007ec55968c7b2decfab775b3afcbd
Nick <nickwalton00@gmail.com>
2019-11-28 20:12:36 -0700

    more stories!

--- data/build_training_data.py
+++ data/build_training_data.py
@@ -17,6 +17,21 @@ def remove_phrase(text):
 def make_stories(current_story, tree):
     stories = []
     action = first_to_second_person(tree["action"])
+    action_list = action.split(" ")
+    first_word = action_list[0]
+    if first_word[-1] == '.':
+        first_word = first_word[:-1]
+
+    dont_add_you = ["the", "another", "next", "in", "monday", "back", "a", "years", "one",
+                    "two", "during", "months", "weeks", "seven", "three", "...", "twelve",
+                    "four","five","six", "blackness...", "you", "no", "yes", "up", "down", "onward", ]
+
+    if action[0] is '"':
+        last_quote = action.rfind('"')
+        action = "You say " + action[:last_quote + 1]
+    elif first_word.lower() not in dont_add_you:
+        action = "You " + action[0].lower() + action[1:]
+
     action = remove_phrase(action)
     result = remove_phrase(tree["result"])
     current_story += ("\n> " + action + "\n" + result)
@@ -43,10 +58,17 @@ def get_stories(filename):
 
 output_file_path = "text_adventures.txt"
 with open(output_file_path, 'w') as output_file:
-    filenames = ["stories/story" + str(i) + ".json" for i in range(0,41)]
+    filenames = ["stories/story" + str(i) + ".json" for i in range(0,93)]
+    for filename in filenames:
+        tree = load_tree(filename)
+        print('"' + tree["tree_id"] + '",')
+
+
+    filenames += ["stories/crowdsourcedstory" + str(i) + ".json" for i in range(0, 12)]
     stories = []
     for filename in filenames:
-        stories += get_stories(filename)
+        filename_stories = get_stories(filename)
+        stories += filename_stories
         print(len(stories))
 
     raw_text = ""
