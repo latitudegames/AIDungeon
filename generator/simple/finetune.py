02565b4a59f8e88c87aa8f6924fbdb686b37126e
root <nickwalton00@gmail.com>
2019-11-30 14:19:07 +0000

    updatefinetune

--- generator/simple/finetune.py
+++ generator/simple/finetune.py
@@ -8,18 +8,18 @@ if not os.path.isdir(os.path.join("models", model_name)):
     print("Downloading ", model_name, " model...")
     gpt2.download_gpt2(model_name=model_name)  # model is saved into current directory under /models/124M/
 
-file_name = "writing_prompts.txt"
+file_name = "text_adventures.txt"
 
 sess = gpt2.start_tf_sess()
 gpt2.finetune(sess,
               file_name,
               multi_gpu=True,
-              batch_size=8,
+              batch_size=32,
               learning_rate=0.0001,
               model_name=model_name,
               sample_every=1000,
-              max_checkpoints=1,
+              max_checkpoints=8,
               save_every=200,
-              steps=600)
+              steps=1000)
 
 gpt2.generate(sess)
