dcc2aca7bfc64ef52d711f681f28ff6693491b14
Nick Walton <nickwalton00@gmail.com>
2019-12-06 10:27:14 -0700

    Develop (#21)
    * various small changes
    
    * various small changes
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * auto save
    
    * change model loc
    
    * updated instructions
    
    * moved to gcloud storage
    
    * fixed cd issue

--- install.sh
+++ install.sh
@@ -7,20 +7,11 @@ if [ -d "${MODELS_DIRECTORY}/${MODEL_VERSION}" ]; then
     echo "AIDungeon2 is already installed"
 
 else
-	echo "Downloading AIDungeon2 Model... (this may take a few minutes)"
     cd ${MODELS_DIRECTORY}
-    mkdir ${MODEL_VERSION}
-    cd ${MODEL_VERSION}
-    apt-get install aria2 > /dev/null
-    aria2c -x 16 -s 32 "${DOWNLOAD_URL}/${MODEL_VERSION}/${MODEL_NAME}.data-00000-of-00001"
-    wget "${DOWNLOAD_URL}/${MODEL_VERSION}/checkpoint" > /dev/null
-    wget "${DOWNLOAD_URL}/${MODEL_VERSION}/encoder.json" > /dev/null
-    wget "${DOWNLOAD_URL}/${MODEL_VERSION}/hparams.json" > /dev/null
-    wget "${DOWNLOAD_URL}/${MODEL_VERSION}/${MODEL_NAME}.index" > /dev/null
-    wget "${DOWNLOAD_URL}/${MODEL_VERSION}/${MODEL_NAME}.meta" > /dev/null
-    wget "${DOWNLOAD_URL}/${MODEL_VERSION}/vocab.bpe" > /dev/null
+    	echo "Downloading AIDungeon2 Model... (this may take a few minutes)"
+    gsutil -m cp -r gs://multiregionaidungeon2/model_v5 .
     echo "Download Complete!"
-    cd ../../../..
+    cd ../../..
 
     pip install -r requirements.txt > /dev/null
 fi
