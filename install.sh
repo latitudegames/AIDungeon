17237774dfbf7426f1534cc8d2adfa26ddec9d26
Nick <nickwalton00@gmail.com>
2019-12-07 16:15:44 -0700

    removed old files and changed download location

--- install.sh
+++ install.sh
@@ -1,18 +1,25 @@
 MODELS_DIRECTORY=generator/gpt2/models
 MODEL_VERSION=model_v5
 MODEL_NAME=model-550
-DOWNLOAD_URL=https://aidungeonmodel.s3-us-west-1.amazonaws.com
+DOWNLOAD_URL=http://130.211.31.182:80
 
 if [ -d "${MODELS_DIRECTORY}/${MODEL_VERSION}" ]; then
     echo "AIDungeon2 is already installed"
 
 else
+	echo "Downloading AIDungeon2 Model... (this may take a few minutes)"
     cd ${MODELS_DIRECTORY}
-    	echo "Downloading AIDungeon2 Model... (this may take a few minutes)"
-    gsutil -m cp -r gs://ai_dungeon_awesomeness/model_v5 .
-
+    mkdir ${MODEL_VERSION}
+    cd ${MODEL_VERSION}
+    aria2c -x 16 -s 32 "${DOWNLOAD_URL}/${MODEL_VERSION}/${MODEL_NAME}.data-00000-of-00001"
+    wget "${DOWNLOAD_URL}/${MODEL_VERSION}/checkpoint" > /dev/null
+    wget "${DOWNLOAD_URL}/${MODEL_VERSION}/encoder.json" > /dev/null
+    wget "${DOWNLOAD_URL}/${MODEL_VERSION}/hparams.json" > /dev/null
+    wget "${DOWNLOAD_URL}/${MODEL_VERSION}/${MODEL_NAME}.index" > /dev/null
+    wget "${DOWNLOAD_URL}/${MODEL_VERSION}/${MODEL_NAME}.meta" > /dev/null
+    wget "${DOWNLOAD_URL}/${MODEL_VERSION}/vocab.bpe" > /dev/null
     echo "Download Complete!"
-    cd ../../..
+    cd ../../../..
 
     pip install -r requirements.txt > /dev/null
 fi
