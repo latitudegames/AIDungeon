cf2cd3f766121e5e42169251e269ded7373498d4
L <lgvichy@gmail.com>
2019-12-10 15:16:56 -0800

    Focus the script to just download torrent
    This avoid coupling multiple function into a single script and allow more flexibility when it comes to getting the model

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 cd "$(dirname "${0}")"
+BASE_DIR="$(pwd)"
+
 declare -A OS_INFO;
 OS_INFO[/etc/debian_version]="apt-get install"
 OS_INFO[/etc/alpine-release]="apk --update add"
@@ -8,12 +10,6 @@ OS_INFO[/etc/fedora-release]="dnf install"
 OS_INFO[/etc/arch-release]="pacman -S"
 PYTHON_VER=$(python --version | sed -En "s/Python //p" | cut -c1-3)
 PACKAGES=(aria2 git unzip wget)
-BASE_DIR="$(pwd)"
-MODELS_DIRECTORY=generator/gpt2/models
-MODEL_VERSION=model_v5
-MODEL_NAME=model-550
-MODEL_TORRENT_URL="https://github.com/AIDungeon/AIDungeon/files/3935881/model_v5.torrent.zip"
-MODEL_TORRENT_BASENAME="$(basename "${MODEL_TORRENT_URL}")"
 
 pip_install () {
 	if [ "$PYTHON_VER" != "3.6" ]; then 
@@ -53,28 +49,6 @@ aid_install () {
 	else
 		sudo ${PACKAGE_MANAGER} ${PACKAGES[@]}
 	fi
-	echo "Creating directories."
-	mkdir -p "${MODELS_DIRECTORY}"
-	cd "${MODELS_DIRECTORY}"
-	mkdir "${MODEL_VERSION}"
-	wget "${MODEL_TORRENT_URL}"
-	unzip "${MODEL_TORRENT_BASENAME}"
-	which aria2c > /dev/null
-	if [ $? == 0 ]; then
-		echo -e "\n\n==========================================="
-		echo "We are now starting to download the model."
-		echo "It will take a while to get up to speed."
-		echo "DHT errors are normal."
-		echo -e "===========================================\n"
-		aria2c \
-			--max-connection-per-server 16 \
-			--split 64 \
-			--bt-max-peers 500 \
-			--seed-time=0 \
-			--summary-interval=15 \
-			--disable-ipv6 \
-			"${MODEL_TORRENT_BASENAME%.*}"
-		echo "Download Complete!"
 		pip_install
 	else
 		echo "aria2c isn't available in PATH."
@@ -82,29 +56,5 @@ aid_install () {
 		exit
 	fi
 }
-
-reinstall () {
-	echo "Deleting $MODELS_DIRECTORY"
-	rm -rf ${MODELS_DIRECTORY}
-	aid_install
-}
-
-if [[ -d "${MODELS_DIRECTORY}/${MODEL_VERSION}" ]]; then
-	ANSWER="n"
-	echo "AIDungeon2 is appears to be installed."
-	echo "Would you like to reinstall?"
-	echo "Warning, this will delete some files![y/N]"
-	read ANSWER
-	ANSWER=$(echo $ANSWER | tr '[:upper:]' '[:lower:]')
-	case $ANSWER in
-		"yes")
-			reinstall;;
-		"y")
-			reinstall;;
-		*)
-			echo "Exiting program!"
-			exit;;
-	esac
-fi
 
 aid_install
