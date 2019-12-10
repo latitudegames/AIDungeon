3e629ae8b7092d9ba7e497c61fad09bbd9a3a2f6
Harry Pollard <harry@meharryp.xyz>
2019-12-10 19:32:37 +0000

    Moved torrent URL to new git repo

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ BASE_DIR="$(pwd)"
 MODELS_DIRECTORY=generator/gpt2/models
 MODEL_VERSION=model_v5
 MODEL_NAME=model-550
-MODEL_TORRENT_URL="https://github.com/nickwalton/AIDungeon/files/3935881/model_v5.torrent.zip"
+MODEL_TORRENT_URL="https://github.com/AIDungeon/AIDungeon/files/3935881/model_v5.torrent.zip"
 MODEL_TORRENT_BASENAME="$(basename "${MODEL_TORRENT_URL}")"
 
 if [[ -d "${MODELS_DIRECTORY}/${MODEL_VERSION}" ]]; then
