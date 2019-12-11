9ab415be1f8369d4390b2f3b61834b3aca122406
GodofGrunts <dergottdergrunten@gmail.com>
2019-12-11 09:09:08 -0500

    Reduced whitespace, fails if aria2c not found, deletes  instead of ./generator.

--- install.sh
+++ install.sh
@@ -16,7 +16,6 @@ MODEL_TORRENT_URL="https://github.com/nickwalton/AIDungeon/files/3935881/model_v
 MODEL_TORRENT_BASENAME="$(basename "${MODEL_TORRENT_URL}")"
 
 pip_install () {
-
 	if [ "$PYTHON_VER" != "3.6" ]; then 
 		echo "One of the packages 'Tensorflow 1.15' only supports Python 3.6.x"
 		echo "Your default Python installation is $PYTHON_VER.x."
@@ -34,15 +33,11 @@ pip_install () {
 		sudo ${PIP36} install --upgrade pip setuptools
 		sudo ${PIP36} install -r $BASE_DIR/requirements.txt
 	fi
-
 	echo "Once everything is installed, play the game with 'python3.6 ./play.py'"
 	exit
-
 }
 
-
 aid_install () {
-
 	echo "Determining package manager."
 	for f in ${!OS_INFO[@]}
 	do
@@ -51,7 +46,6 @@ aid_install () {
 			PACKAGE_MANAGER=${OS_INFO[$f]}
 		fi
 	done
-
 	if [ -z "$PACKAGE_MANAGER" ]; then
 		echo "You do not seem to be using a supported package manager."
 		echo "Please make sure ${PACKAGES[@]} are installed then press [ENTER]"
@@ -59,37 +53,39 @@ aid_install () {
 	else
 		sudo ${PACKAGE_MANAGER} ${PACKAGES[@]}
 	fi
-
-
-
 	echo "Creating directories."
 	mkdir -p "${MODELS_DIRECTORY}"
 	cd "${MODELS_DIRECTORY}"
 	mkdir "${MODEL_VERSION}"
 	wget "${MODEL_TORRENT_URL}"
 	unzip "${MODEL_TORRENT_BASENAME}"
-	echo -e "\n\n==========================================="
-	echo "We are now starting to download the model."
-	echo "It will take a while to get up to speed."
-	echo "DHT errors are normal."
-	echo -e "===========================================\n"
-	aria2c \
-		--max-connection-per-server 16 \
-		--split 64 \
-		--bt-max-peers 500 \
-		--seed-time=0 \
-		--summary-interval=15 \
-		--disable-ipv6 \
-		"${MODEL_TORRENT_BASENAME%.*}"
-	echo "Download Complete!"
-
-	pip_install
-							
+	which aria2c > /dev/null
+	if [ $? == 0 ]; then
+		echo -e "\n\n==========================================="
+		echo "We are now starting to download the model."
+		echo "It will take a while to get up to speed."
+		echo "DHT errors are normal."
+		echo -e "===========================================\n"
+		aria2c \
+			--max-connection-per-server 16 \
+			--split 64 \
+			--bt-max-peers 500 \
+			--seed-time=0 \
+			--summary-interval=15 \
+			--disable-ipv6 \
+			"${MODEL_TORRENT_BASENAME%.*}"
+		echo "Download Complete!"
+		pip_install
+	else
+		echo "aria2c isn't available in PATH."
+		echo "Exiting."
+		exit
+	fi
 }
 
 reinstall () {
-	echo "Deleting ./generator/*"
-	rm -r ./generator
+	echo "Deleting $MODELS_DIRECTORY"
+	rm -rf ${MODELS_DIRECTORY}
 	aid_install
 }
 
@@ -100,7 +96,6 @@ if [[ -d "${MODELS_DIRECTORY}/${MODEL_VERSION}" ]]; then
 	echo "Warning, this will delete some files![y/N]"
 	read ANSWER
 	ANSWER=$(echo $ANSWER | tr '[:upper:]' '[:lower:]')
-
 	case $ANSWER in
 		"yes")
 			reinstall;;
