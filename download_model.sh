949f604182502ebae5850b25dfb5572e918e3489
Benjamin Bay <48391872+ben-bay@users.noreply.github.com>
2019-12-13 13:44:44 -0800

    Update download_model.sh

--- download_model.sh
+++ download_model.sh
@@ -6,7 +6,7 @@ BASE_DIR="$(pwd)"
 MODELS_DIRECTORY=generator/gpt2/models
 MODEL_VERSION=model_v5
 
-MODEL_DIRECTORY="${MODELS_DIRECTORY}/${MODEL_VERSION}"
+MODEL_DIRECTORY="${MODELS_DIRECTORY}"
 
 MODEL_NAME=model-550
 MODEL_TORRENT_URL="https://github.com/AIDungeon/AIDungeon/files/3935881/model_v5.torrent.zip"
