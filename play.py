76793398111de0ff379d2aedad8247aa2e13beff
LajosTueur <geonczeol.boti@gmail.com>
2019-12-21 18:07:51 +0100

    Added apocalyptic setting to random game #2

--- play.py
+++ play.py
@@ -26,13 +26,11 @@ def random_story(story_data):
     # random setting
     settings = story_data["settings"].keys()
     n_settings = len(settings)
+    n_settings = 2
     rand_n = random.randint(0, n_settings - 1)
     for i, setting in enumerate(settings):
         if i == rand_n:
             setting_key = setting
-
-    # temporarily only available in fantasy
-    setting_key = "fantasy"
 
     # random character
     characters = story_data["settings"][setting_key]["characters"]
@@ -43,7 +41,7 @@ def random_story(story_data):
             character_key = character
 
     # random name
-    name = grammars.direct(setting_key, "fantasy_name")
+    name = grammars.direct(setting_key, "character_name")
 
     return setting_key, character_key, name, None, None
 
